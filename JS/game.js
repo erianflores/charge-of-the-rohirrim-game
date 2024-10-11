698d4ae404eed2c292c8601276fb45f931f8455e
Erian Flores <erian.flores@gmail.com>
2024-10-12 00:36:31 +0200

    Updated

--- JS/game.js
+++ JS/game.js
@@ -3,10 +3,11 @@ class Game {
     this.startScreen = document.querySelector("#game-intro");
     this.gameScreen = document.querySelector("#game-screen");
     this.endScreen = document.querySelector("#end-screen");
-    this.player = new Player(110, 300, 100, 100, "../Images/King Theoden.png");
+    this.player = new Player(90, 280, 80, 80, "../Images/King Theoden.png");
 
     this.height = 600;
     this.width = 1000;
+    this.obstacle = [new Obstacle()];
   }
 
   start() {
---
+++ JS/obstacle.js
@@ -0,0 +1,31 @@
+class Obstacle {
+  constructor() {
+    this.gameScreen = document.querySelector("#game-screen");
+    // this.positionsY = [50 - 550];
+    // this.randomIndex = Math.floor(Math.random() * this.positionsY.length);
+    // this.top = this.positionsY[this.randomIndex];
+    const minY = 50;
+    const maxY = 550;
+    this.top = Math.floor(Math.random() * (maxY - minY + 1)) + minY;
+    this.left = 800;
+    this.width = 100;
+    this.height = 80;
+    this.element = document.createElement("img");
+    this.element.style.position = "absolute";
+    this.element.src = "../Images/Nazgul Fying.png";
+    this.element.style.height = `${this.height}px`;
+    this.element.style.width = `${this.width}px`;
+    this.element.style.top = `${this.top}px`;
+    this.element.style.left = `${this.left}px`;
+    this.gameScreen.appendChild(this.element);
+  }
+  move() {
+    this.left += 3;
+    this.updatePosition();
+  }
+
+  updatePosition() {
+    this.element.style.top = `${this.top}px`;
+    this.element.style.left = `${this.left}px`;
+  }
+}
